362d7e92dd09941668e09bbc6c9cd930f8ea5e1d
Necip <sagiroglun22@itu.edu.tr>
2023-10-20 12:49:34 +0300

    feat: example

--- controllers/index/get.js
+++ controllers/index/get.js
@@ -13,5 +13,22 @@ module.exports = (req, res) => {
         twitter: true
       }
     },
+    example: [
+      {
+        text: 'Example 1',
+        link: '/example1',
+        image: '/img/example1.png'
+      },
+      {
+        text: 'Example 2',
+        link: '/example2',
+        image: '/img/example2.png'
+      },
+      {
+        text: 'Example 3',
+        link: '/example3',
+        image: '/img/example3.png'
+      }
+    ]
   });
 };
